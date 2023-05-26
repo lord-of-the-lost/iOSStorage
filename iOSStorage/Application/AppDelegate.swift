98dfff3ed676c4e9fe71340996b0702fbfd2cf8c
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-05-26 21:43:32 +0300

    add DocumentsVC

--- iOSStorage/Application/AppDelegate.swift
+++ iOSStorage/Application/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let navigationCOntroller = UINavigationController(rootViewController: DocumentsViewController())
+        window?.rootViewController = navigationCOntroller
         window?.makeKeyAndVisible()
         
         return true
