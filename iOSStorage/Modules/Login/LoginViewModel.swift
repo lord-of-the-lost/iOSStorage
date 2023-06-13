cecc302c0da825d1ca5a6663635f950dade6f563
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-06-13 23:48:04 +0300

    refactoring

--- iOSStorage/Modules/Login/LoginViewModel.swift
+++ iOSStorage/Modules/Login/LoginViewModel.swift
@@ -7,33 +7,46 @@
 
 import Foundation
 
-//protocol LoginViewModelProtocol {
-//    var onStateDidChange: ((LoginViewModel.State) -> Void)? { get set }
-//    func updateState(viewInput: LoginViewModel.ViewInput)
-//}
-//
-//final class LoginViewModel: LoginViewModelProtocol {
-//    enum State {
-//        case initial
-//        case repeatPassword
-//    }
-//    
-//    enum ViewInput {
-//        case passwordRecived
-//    }
-//    
-//    var onStateDidChange: ((State) -> Void)?
-//    
-//    private(set) var state: State = .initial {
-//        didSet {
-//            onStateDidChange?(state)
-//        }
-//    }
-//    
-//    func updateState(viewInput: ViewInput) {
-//        switch viewInput {
-//        case .addNewMessege:
-//            state = .loadMesseges
-//        }
-//    }
-//}
+protocol LoginViewModelProtocol {
+    var onStateDidChange: ((LoginViewModel.State) -> Void)? { get set }
+    func getCurrentState() -> LoginViewModel.State
+    func getInitialPassword() -> String?
+    func updateState(viewInput: LoginViewModel.ViewInput)
+}
+
+final class LoginViewModel: LoginViewModelProtocol {
+    enum State {
+        case initial
+        case repeatPassword
+    }
+    
+    enum ViewInput {
+        case passwordReceived(String)
+        case passwordReset
+    }
+    
+    var onStateDidChange: ((State) -> Void)?
+    
+    private var state: State = .initial
+    private var initialPassword: String?
+    
+    func getCurrentState() -> State {
+        return state
+    }
+    
+    func getInitialPassword() -> String? {
+        return initialPassword
+    }
+    
+    func updateState(viewInput: ViewInput) {
+        switch viewInput {
+        case .passwordReceived(let password):
+            initialPassword = password
+            state = .repeatPassword
+        case .passwordReset:
+            initialPassword = nil
+            state = .initial
+        }
+        onStateDidChange?(state)
+    }
+}
